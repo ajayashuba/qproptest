736536c37da7c7cd45c4b74f1086704c35ddcbfd
ajay kumar <ajay.ashubaba@gmail.com>
2019-01-08 09:28:13 +0000

    n

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -33,29 +33,10 @@
         inset-delimiter
       >
         <q-list-header>Essential Links</q-list-header>
-        <q-item @click.native="openURL('http://quasar-framework.org')">
-          <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
-        </q-item>
-        <q-item @click.native="openURL('https://github.com/quasarframework/')">
-          <q-item-side icon="code" />
-          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-        </q-item>
-        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-          <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-        </q-item>
-        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
-        </q-item>
+       
         <q-item to='prop/ab1/cd2'>
           <q-item-side icon="rss feed" />
-          <q-item-main label="lockmaster" sublabel="@quasarframework" />
+          <q-item-main label="test prop click"  />
         </q-item>
       </q-list>
     </q-layout-drawer>
